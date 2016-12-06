1c0e612383ddf4510f7ad8199594014cacf5f281
twof <fabiobean2@gmail.com>
2016-12-06 13:57:00 -0800

    added pods and bridging header

---
+++ LightHouse/ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  LightHouse
+//
+//  Created by fnord on 12/6/16.
+//  Copyright © 2016 Sam Lee. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
---
+++ Pods/Target Support Files/Pods-LightHouse/Pods-LightHouse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LightHouse : NSObject
+@end
+@implementation PodsDummy_Pods_LightHouse
+@end
