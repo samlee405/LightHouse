4e5a8e5515311f4b38d0b61ee7657a3174e785f5
twof <fabiobean2@gmail.com>
2016-12-06 19:17:36 -0800

    Added helpers and such

---
+++ LightHouse/EstimoteHelper.swift
@@ -0,0 +1,20 @@
+//
+//  EstimoteHelper.swift
+//  LightHouse
+//
+//  Created by fnord on 12/6/16.
+//  Copyright © 2016 Sam Lee. All rights reserved.
+//
+
+import Foundation
+
+////class EstimoteHelper: ESTDeviceManagerDelegate, ESTDeviceConnectableDelegate {
+////    
+////    var deviceManager: ESTDeviceManager!
+////    var device: ESTDeviceLocationBeacon!
+////    
+////    
+////    func getNearestEstimote()->ESTDevice{
+////        
+////    }
+//}
--- LightHouse/Room.swift
+++ LightHouse/Room.swift
@@ -10,10 +10,32 @@ import Foundation
 
 class Room {
     var roomTitle: String
-    var roomBeacon: String?
+    var roomBeacon: ESTDevice
     var roomLights = [Light]()
     
-    init(name: String) {
-        roomTitle = name
+    init(name: String, roomBeacon: ESTDevice?) {
+        self.roomTitle = name
+        self.roomBeacon = roomBeacon ?? ESTDevice()
+    }
+    
+    // What adding a light to a room really does is add the uuid of the light to the broadcast string of the estimote associated with the room
+    func addLightToRoom(light: Light){
+        roomLights.append(light)
+        
+        var broadcastString = roomLightStringBuilder()
+        
+        
+    }
+    
+    private func roomLightStringBuilder() -> String{
+        var broadcastString = "" //consists of the room name followed by a list of room light identifiers (formatted as JSON maybe?)
+        
+        broadcastString.append(roomTitle)
+        
+        for light in self.roomLights{
+            //Takes the UUID of the light and adds it to the broadcast string
+        }
+        
+        return broadcastString
     }
 }
